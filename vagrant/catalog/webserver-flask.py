7712a2fc107621faa0bbe168f8cd9dfbfc627082
Tim Nelson <tdnelson@gmail.com>
2017-07-04 12:16:37 -0700

    Impliment flask's "url_for()"

--- vagrant/catalog/webserver-flask.py
+++ vagrant/catalog/webserver-flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -15,15 +15,22 @@ session = DBSession()
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
-    output = ""
-    for item in items:
-        output += item.name
-        output += "<br>"
-        output += item.price
-        output += "<br>"
-        output += item.description
-        output += "<br><br>"
-    return output
+    return render_template("menu.html", restaurant=restaurant, items=items)
+
+
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
 
 if __name__ == "__main__":
     app.debug = True
