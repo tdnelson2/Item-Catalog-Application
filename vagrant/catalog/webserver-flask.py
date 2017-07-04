045b31f7b79de8355f1b12d619fab9f2a621aa3d
Tim Nelson <tdnelson@gmail.com>
2017-07-03 20:34:00 -0700

    Import db into flask app

---
+++ vagrant/catalog/webserver-flask.py
@@ -0,0 +1,30 @@
+from flask import Flask
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import func
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+@app.route("/")
+@app.route("/hello")
+def HelloWorld():
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ""
+    for item in items:
+        output += item.name
+        output += "<br>"
+        output += item.price
+        output += "<br>"
+        output += item.description
+        output += "<br><br>"
+    return output
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
