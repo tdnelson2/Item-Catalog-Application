a01fce235393088c9ccc1442a535f0624d26ebd2
Tim Nelson <tdnelson@gmail.com>
2017-07-03 20:58:02 -0700

    Add decorators to CRUD methods to parse url components into variables

--- vagrant/catalog/webserver-flask.py
+++ vagrant/catalog/webserver-flask.py
@@ -11,10 +11,10 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 @app.route("/")
-@app.route("/hello")
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+@app.route("/restaurants/<int:restaurant_id>/")
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
     output = ""
     for item in items:
         output += item.name
