a20b175d21e9c55d467455cf2dad8e88da614465
Tim Nelson <tdnelson@gmail.com>
2017-09-28 13:19:33 -0700

    shorten dummy data description

--- gregslist_raw_dummy_data.py
+++ gregslist_raw_dummy_data.py
@@ -1,10 +1,4 @@
-description =      ("Far far away, behind the word mountains, "
-                    "far from the countries Vokalia and Consonantia, "
-                    "there live the blind texts. Separated they live in "
-                    "Bookmarksgrove right at the coast of the Semantics, "
-                    "a large language ocean. A small river named Duden "
-                    "flows by their place and supplies it with the "
-                    "necessary regelialia.")
+description =      "Far far away, behind the word mountains..."
 
 job_categories =   ["finance",
                     "office",
